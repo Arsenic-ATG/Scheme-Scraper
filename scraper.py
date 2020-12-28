6256e91b0a0d3bcc1b77ee5efb251d237305d5f1
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-12-28 19:51:27 +0530

    Fetch all the headings of the Schemes

--- scraper.py
+++ scraper.py
@@ -1,1 +1,19 @@
+#! python3
+
 # Web Scraper which will scrape latest schemes from the government website when run 
+ 
+import requests, bs4, json
+
+url = 'https://krishijagran.com/agripedia/best-government-schemes-and-programmes-in-agriculture-for-farmers/'
+
+# Get the page and check for any HTTP error while doing so
+res = requests.get(url)
+res.raise_for_status()
+
+# Retrieve the data of the site
+soup = bs4.BeautifulSoup(res.text,'lxml')
+
+# Fetch all the headings of the Schemes
+for child in soup.find_all("h3"):
+	if(child.contents[0].name == "strong"):
+		print(child.string)
