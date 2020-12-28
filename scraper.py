da922acf13da544118d8f34bb5b22584fc3b35ea
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-12-28 20:53:20 +0530

    Put all the data in json file
    now scraper can put all the output in a json file to be used in the app

--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@
 import requests, bs4, json
 
 url = 'https://krishijagran.com/agripedia/best-government-schemes-and-programmes-in-agriculture-for-farmers/'
+data = {}
+data['scheme'] = []
 
 # Get the page and check for any HTTP error while doing so
 res = requests.get(url)
@@ -16,4 +18,20 @@ soup = bs4.BeautifulSoup(res.text,'lxml')
 # Fetch all the headings of the Schemes
 for child in soup.find_all("h3"):
 	if(child.contents[0].name == "strong"):
-		print(child.string)
+		# print(child.string)
+		data['scheme'].append({
+		    'title': child.string,
+		    'details': 'will put detais here',
+		    'link': 'will put link here if found any'
+		})
+
+# fetching detais for the file
+# for details in soup.find_all("p"):
+# 	if(details.previous_sibling.name == "h3"):
+# 		print(details.string)
+# 	else:
+# 		print("not found");
+
+# putting stuff in json file
+with open('results.json', 'w') as outfile:
+    json.dump(data, outfile)
